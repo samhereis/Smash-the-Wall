7eeb3777155b4b3aa9c4f5cd3fc95f671949610c
samhereis <samagan888@gmail.com>
2023-08-10 19:11:10 +0600

    living block enviroment

--- Smash the Wall/Assets/Scripts/Managers/EnviromentsManager.cs
+++ Smash the Wall/Assets/Scripts/Managers/EnviromentsManager.cs
@@ -1,33 +1,90 @@
+using DI;
 using Helpers;
 using Identifiers;
+using InGameStrings;
+using Interfaces;
+using PlayerInputHolder;
+using System;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
+using UnityEngine.InputSystem;
 
 namespace Managers
 {
-    public sealed class EnviromentsManager : MonoBehaviour
+    public sealed class EnviromentsManager : MonoBehaviour, IDIDependent, IInitializable<int>, IClearable
     {
+        [Header("DI")]
+        [DI(DIStrings.inputHolder)] private Input_SO _input;
+
         [Header("Components")]
         [SerializeField] private AssetReferenceGameObject[] _suitableEnviroments;
 
         [Header("Debug")]
         [SerializeField] private EnviromentIdentifier _enviroment;
+        [SerializeField] private int _currentIndex = 0;
 
-        private async void Awake()
+        private void Start()
         {
-            AssetReferenceGameObject enviroment = _suitableEnviroments[Random.Range(0, _suitableEnviroments.Length)];
+            (this as IDIDependent).LoadDependencies();
+
+            Initialize(_currentIndex);
+
+            _input.input.Player.ChangeWeapon.performed += ChangeEnviroment;
+        }
+
+        private void OnDestroy()
+        {
+            Clear();
+
+            _input.input.Player.ChangeWeapon.performed -= ChangeEnviroment;
+        }
+
+        private void ChangeEnviroment(InputAction.CallbackContext context)
+        {
+            Clear();
+
+            _currentIndex++;
+
+            if (_currentIndex >= _suitableEnviroments.Length)
+            {
+                _currentIndex = 0;
+            }
+
+            Initialize(_currentIndex);
+        }
+
+        public async void Initialize(int index)
+        {
+            AssetReferenceGameObject enviroment = _suitableEnviroments[index];
 
             _enviroment = await AddressablesHelper.InstantiateAsync<EnviromentIdentifier>(enviroment);
 
-            if (enviroment != null)
+            if (_enviroment != null)
             {
                 DontDestroyOnLoad(_enviroment);
             }
         }
 
-        private void OnDestroy()
+        public void Clear()
         {
-            AddressablesHelper.DestroyObject(_enviroment.gameObject);
+            Delete(_enviroment);
+
+            foreach (var enviroment in FindObjectsOfType<EnviromentIdentifier>(true))
+            {
+                Delete(enviroment);
+            }
+
+            void Delete(EnviromentIdentifier enviroment)
+            {
+                try
+                {
+                    AddressablesHelper.DestroyObject(enviroment.gameObject);
+                }
+                catch (Exception exception)
+                {
+                    Debug.LogWarning("Could not delete enviroment: " + exception.Message);
+                }
+            }
         }
     }
 }
